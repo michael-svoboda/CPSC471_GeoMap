6c1af06c05d52dabac8fc370c905cc6da839b233
Murtaza1Mustafa <117037991+Murtaza1Mustafa@users.noreply.github.com>
2023-04-05 16:35:58 -0600

    added functions for gas and prod to production tab

--- client/src/scenes/production/index.jsx
+++ client/src/scenes/production/index.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
 import Box from '@mui/material/Box';
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
+
 
 
 const headerData = [
@@ -59,61 +61,9 @@ const headerData = [
       "RRDate": "1995-12-07",
       "Stat": "ABD Zn"
     },
-    {
-        "UWI": "100/06-04-058-14W5/00",
-        "WellName": "BSRL MCLEOD 6-4-58-14",
-        "Lahee": "DEV",
-        "LicNum": 180405,
-        "LicDate": "1995-08-31",
-        "CurOperator": "Blue Sky Rsrcs Ltd",
-        "KB": 1064.6,
-        "GL": 1060.5,
-        "TrueVDpth": 2177,
-        "MD": 2177,
-        "FM_at_TVD": "Jnordegg",
-        "DirFlag": "Vertical",
-        "SpudDate": "1995-09-07",
-        "CompDate": "1995-09-16",
-        "RRDate": "1995-09-19",
-        "Stat": "Flow GAS"
-      },
-      {
-        "UWI": "100/10-05-058-14W5/00",
-        "WellName": "HAMILTON HOME SHINBK 10-5-58-14",
-        "Lahee": "NFW",
-        "LicNum": 56535,
-        "LicDate": "1976-01-12",
-        "CurOperator": "Hamilton Bros Cdn Gas",
-        "KB": 1038.8,
-        "GL": 1034.8,
-        "TrueVDpth": 2267.7,
-        "MD": 2267.7,
-        "FM_at_TVD": "Mbanff",
-        "DirFlag": "Vertical",
-        "SpudDate": "1976-01-13",
-        "CompDate": "1976-01-26",
-        "RRDate": "*",
-        "Stat": "ABD"
-      },
-      {
-        "UWI": "100/06-07-058-14W5/00",
-        "WellName": "SERENPET WINDFALL 6-7-58-14",
-        "Lahee": "NPW",
-        "LicNum": 182059,
-        "LicDate": "1995-11-13",
-        "CurOperator": "Repsol O&G Cda Inc",
-        "KB": 1077.6,
-        "GL": 1072.7,
-        "TrueVDpth": 2203.3,
-        "MD": 2204,
-        "FM_at_TVD": "Jnordegg",
-        "DirFlag": "Vertical",
-        "SpudDate": "1995-11-15",
-        "CompDate": "1995-12-03",
-        "RRDate": "1995-12-07",
-        "Stat": "ABD Zn"
-      },
 ]
+
+
 
 const columns = [
     { field: 'UWI', headerName: 'UWI', width: 200 },
@@ -138,9 +88,55 @@ const columns = [
   ];
 
 const ProductionChart = () => {
+  const [prodset, setProdDataset] = React.useState([]);
+  const [gasset, setGasDataset] = React.useState([]);
+  async function fetchProdData() {
+
+    try {
+      console.log("hater");
+      const response = await fetch('http://localhost:5000/prod-data');
+      const DataJson = await response.json();
+      const Data = DataJson.map(item => item.Data);
+      console.log(DataJson);
+      if(DataJson!=undefined){
+        setProdDataset(DataJson);
+      }
+      
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  }
+  async function fetchGasData() {
+
+    try {
+      console.log("lover");
+      const response = await fetch('http://localhost:5000/gas-data');
+      const DataJson = await response.json();
+      const Data = DataJson.map(item => item.Data);
+      console.log(DataJson);
+      if(DataJson!=undefined){
+        setGasDataset(DataJson);
+      }
+      
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  }
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
   const chartRef = useRef(null);
   const gasChartRef = useRef(null);
-  const liquidChartRef = useRef(null);
+  const dates = [];
+  const oilprod = [];
+  const gasprod = [];
+  const waterprod = [];
+  var C1 = 0;
+  var C2 = 0;
+  var C3 = 0;
+  var C7P = 0; 
+  var N2 = 0;
+  var CO2 = 0;
   const gasData = {
     labels: ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7+', 'N2', 'CO2'],
     datasets: [
@@ -152,41 +148,49 @@ const ProductionChart = () => {
       },
     ],
   };
-  const liquidData = {
-    labels: ['Oil', 'Water'],
-    datasets: [
-      {
-        label: 'Liquid Composition',
-        data: [80, 20],
-        backgroundColor: ['rgba(255,99,132,0.4)', 'rgba(54,162,235,0.4)'],
-        borderWidth: 1,
-      },
-    ],
-  };
 
   useEffect(() => {
+    fetchProdData();
+    fetchGasData();
+    if(prodset!=[]){ 
+      for(let i = 0; i < prodset.length; i++){
+        dates.push(prodset[i].Date);
+        oilprod.push(prodset[i].OIL);
+        gasprod.push(prodset[i].GAS);
+        waterprod.push(prodset[i].WATER);
+      }
+    }
+    if(gasset!=[]){/*
+      C1 = (gasset[0].avg_C1*100);
+      C2 = (gasset[0].avg_C2*100);
+      C3 = (gasset[0].avg_C3*100);
+      C7P = (gasset[0].avg_C7P*100)
+      N2 = (gasset[0].avg_N2*100);
+      CO2 = (gasset[0].avg_C02*100);  */
+    }
+
     const productionChart = new Chart(chartRef.current, {
       type: 'line',
       data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: dates,
         datasets: [
           {
             label: 'Oil Production',
-            data: [65, 59, 50, 40, 35, 28, 25, 20, 18, 15, 12, 10, 8, 6, 5, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            data: oilprod,
             fill: false,
             borderColor: 'red',
             tension: 0.1,
           },
           {
             label: 'Water Production',
-            data: [3, 3.5, 4, 4.2, 4.5, 5, 5.5, 5.8, 6, 6.2, 6.4, 6.6, 6.8, 7, 7.2, 7.4, 7.6, 7.8, 8, 8.2, 8.4, 8.6, 8.8, 9, 9.2, 9.4, 9.6, 9.8, 10, 10.2],
+            data: waterprod,
             fill: false,
             borderColor: 'blue',
             tension: 0.1,
           },
           {
             label: 'Gas Production',
-            data: [5, 20, 18, 15, 12, 10, 8, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            data: gasprod,
             fill: false,
             borderColor: 'green',
             tension: 0.1,
@@ -198,11 +202,11 @@ const ProductionChart = () => {
     const gasChart = new Chart(gasChartRef.current, {
       type: 'bar',
       data: {
-        labels: ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7+', 'N2', 'CO2'],
+        labels: ['C1', 'C2', 'C3',  'C7+', 'N2', 'CO2'],
         datasets: [
           {
             label: 'Gas Composition',
-            data: [20, 12, 10, 8, 5, 3, 2, 35, 5],
+            data: [C1, C2, C3, C7P, N2, CO2],
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
@@ -233,50 +237,9 @@ const ProductionChart = () => {
         },
       },
     });
-  
-    const liquidChart = new Chart(liquidChartRef.current, {
-      type: 'bar',
-      data: {
-        labels: ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7+', 'N2', 'CO2'],
-        datasets: [
-          {
-            label: 'Liquid Composition',
-            data: [50, 20, 15, 7, 5, 2, 1, 0, 0],
-            backgroundColor: 'rgba(255, 206, 86, 0.2)',
-            borderColor: 'rgba(255, 206, 86, 1)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Composition (%)',
-              font: {
-                size: 16,
-              },
-            },
-          },
-          x: {
-            title: {
-              display: true,
-              text: 'Component',
-              font: {
-                size: 16,
-              },
-            },
-          },
-        },
-      },
-    });
-  
     return () => {
       productionChart.destroy();
       gasChart.destroy();
-      liquidChart.destroy();
     };
   }, []);
   
