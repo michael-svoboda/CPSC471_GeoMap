d03cf9bb79f7ce7853a0a5fec9ae8908412f4c40
Murtaza1Mustafa <117037991+Murtaza1Mustafa@users.noreply.github.com>
2023-04-04 22:28:04 -0600

    added UpdateSelected

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ const saveWellGroupToDB = require('./routes/saveWellGroupToDB');
 const deleteWellGroupFromDB = require('./routes/deleteWellGroupFromDB');
 const distinctGroupList = require('./routes/distinctGroupList');
 const distinctWellList = require('./routes/distinctWellList');
+const UpdateSelectedWells = require('./routes/UpdateSelectedWells');
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 
 const app = express();
 const sqlHandler = new SQLHandler();
@@ -19,21 +21,33 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
-*/
+}); 
+let Users = [
+  {
+    "email": "chungus@email.com",
+    "password": "peepee",
+  },
+  {
+    "email": "amongus@gmail.com",
+    "password": "poopoo",
+  }];
 
+app.get("/api", (req, res) => {
+  res.json({Users})
+})
 app.use('/vert-wells', vertWellListRouter(sqlHandler));
 app.use('/horz-wells', horzWellsRouter(sqlHandler));
 app.use('/selected-wells', selectedWellsRouter());
 app.use('/save-well-group',saveWellGroupToDB());
 app.use('/delete-well-group', deleteWellGroupFromDB());
-app.use('/distinct-groups', distinctGroupList())
-app.use('/distinct-wells', distinctWellList())
+app.use('/distinct-groups', distinctGroupList());
+app.use('/distinct-wells', distinctWellList());
+app.use('/selection-update', UpdateSelectedWells());
 
 app.listen(5000, () => console.log('Server started on port 5000.'));
